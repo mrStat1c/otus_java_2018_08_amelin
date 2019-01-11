cb039c5732ff1e748210093654a2213fb8cf3e4b
mr_Static <amelin.ilya@gmail.com>
2019-01-11 22:41:21 +0300

    hw13

--- HW13/src/main/java/ru/otus/ArrayUtils.java
+++ HW13/src/main/java/ru/otus/ArrayUtils.java
@@ -1,6 +1,56 @@
 package ru.otus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayUtils {
+
+    public static int[] parallelSort(int[] array, int threadCount) throws InterruptedException {
+
+        int partedArraySize;
+        int lastPartedArraySize;
+        List<int[]> partedArrays = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
+        if (array.length % threadCount == 0) {
+            partedArraySize = array.length / threadCount;
+            lastPartedArraySize = partedArraySize;
+        } else {
+            partedArraySize = array.length / threadCount + 1;
+            lastPartedArraySize = array.length - (threadCount - 1) * partedArraySize;
+        }
+
+
+        for (int i = 1; i <= threadCount; i++) {
+            int[] localArray;
+            if (i == threadCount) {
+                localArray = new int[lastPartedArraySize];
+            } else {
+                localArray = new int[partedArraySize];
+            }
+            System.arraycopy(array, partedArraySize * (i - 1), localArray, 0, localArray.length);
+
+            threads.add(new Thread(() -> {
+                Arrays.sort(localArray);
+                partedArrays.add(localArray);
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        int[] sortedArray = new int[0];
+        for (int[] partedArray : partedArrays) {
+            sortedArray = ArrayUtils.merge(sortedArray, partedArray);
+        }
+        return sortedArray;
+    }
+
 
     public static int[] merge(int[] array1, int[] array2) {
         int array1Length = array1.length;
@@ -19,4 +69,5 @@ public class ArrayUtils {
         }
         return result;
     }
+
 }
--- HW13/src/main/java/ru/otus/Main.java
+++ HW13/src/main/java/ru/otus/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         int[] mas = new int[100000];
         Random random = new Random();
@@ -13,34 +13,10 @@ public class Main {
             mas[i] = random.nextInt(1000);
         }
 
-        int[] mas1 = new int[25000];
-        int[] mas2 = new int[25000];
-        int[] mas3 = new int[25000];
-        int[] mas4 = new int[25000];
+        mas = ArrayUtils.parallelSort(mas, 4);
 
-        System.arraycopy(mas, 0, mas1, 0, 25000);
-        System.arraycopy(mas, 25000, mas2, 0, 25000);
-        System.arraycopy(mas, 50000, mas3, 0, 25000);
-        System.arraycopy(mas, 75000, mas4, 0, 25000);
-
-        SortedArrayThread thread1 = new SortedArrayThread(mas1);
-        SortedArrayThread thread2 = new SortedArrayThread(mas2);
-        SortedArrayThread thread3 = new SortedArrayThread(mas3);
-        SortedArrayThread thread4 = new SortedArrayThread(mas4);
-
-        thread1.run();
-        thread2.run();
-        thread3.run();
-        thread4.run();
-        mas1 = thread1.getArray();
-        mas2 = thread2.getArray();
-        mas3 = thread3.getArray();
-        mas4 = thread4.getArray();
-
-        mas = ArrayUtils.merge(ArrayUtils.merge(ArrayUtils.merge(mas1, mas2), mas3), mas4);
-
-        for (int i = 0; i < mas.length; i++){
-            System.out.println(mas[i]);
-        }
+        for (int element : mas) {
+            System.out.println(element);
+            }
     }
 }
